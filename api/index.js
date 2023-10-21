8f8227628afcb3d4d0b36dd407efabfdba44c1e0
chamok51 <117306426+chamok51@users.noreply.github.com>
2023-10-21 16:58:08 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,6 +18,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
+
+app.get('/', (req,res)=>{
+  res.send("Hello World)
+})
 app.use('/api/v1/', authRoutes)
 app.use('/api/v1/', userRoutes)
 
